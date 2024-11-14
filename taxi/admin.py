c472b0426bc4d9d20f7c4e4f257819a665a1a109
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-11-14 18:03:09 +0200

    Solution

---
+++ taxi/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from taxi.models import Driver, Manufacturer, Car
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("license_number",)}),)
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("model", "manufacturer", "get_drivers",)
+    search_fields = ("model",)
+    list_filter = ("manufacturer",)
+
+    def get_drivers(self, obj):
+        return ", ".join([driver.username for driver in obj.drivers.all()])
+
+    get_drivers.short_description = "Drivers"
+
+
+@admin.register(Manufacturer)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ("name", "country",)
+    list_filter = ("country",)
---
+++ taxi/models.py
@@ -0,0 +1,48 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Manufacturers"
+        ordering = ("name",)
+
+    def __str__(self) -> str:
+        return f"{self.name} (country: {self.country})"
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name="cars"
+    )
+    drivers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="cars"
+    )
+
+    class Meta:
+        verbose_name_plural = "Cars"
+        ordering = ("model",)
+
+    def __str__(self) -> str:
+        return (
+            f"{self.model} (manufacturer: {self.manufacturer}, drivers: {self.drivers})"
+        )
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Drivers"
+        ordering = ("username",)
+
+    def __str__(self):
+        return f"{self.username}: ({self.first_name} {self.last_name})"
